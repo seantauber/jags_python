cf79e4cca9ecfbae7dbd04cf64df7db0b96c4f8e
Sean Tauber <seanetauber@gmail.com>
2016-01-27 13:02:43 -0800

    added a setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='jags_python',
+      version='0.10',
+      description='python utilities for using JAGS',
+      url='http://github.com/seantauber/jags_python',
+      author='Sean Tauber',
+      author_email='seanetauber@gmail.com',
+      license='MIT',
+      packages=['jags_python'])
